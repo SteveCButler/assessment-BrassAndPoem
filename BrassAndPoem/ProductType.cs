90416dd898c15830a00b432b48f5814b4f0cda3d
Steve Butler <butlersteve.c@gmail.com>
2023-07-17 19:09:01 -0500

    create ProductType class file and setup structure

--- BrassAndPoem/ProductType.cs
+++ BrassAndPoem/ProductType.cs
@@ -1,1 +1,14 @@
-//create your ProductType class here
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrassAndPoem
+{
+    internal class ProductType
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+    }
+}
